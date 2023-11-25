9413eb701aa0ee32010eec78d7bd1851ff245d88
MuhammadKhan <mhk42@njit.edu>
2023-11-24 20:46:56 -0500

    minor fixes

--- public_html/Project/details.php
+++ public_html/Project/details.php
@@ -2,7 +2,7 @@
 require(__DIR__ . "/../../partials/nav.php");
 
 if (is_logged_in(true)) {
-    // Comment this out if you don't want to see the session variables
+    // Log session data if the user is logged in
     error_log("Session data: " . var_export($_SESSION, true));
 }
 
@@ -16,20 +16,20 @@ if (!$dog_id) {
     // Handle the case where no dog ID is provided
     flash("Dog ID not Provided", "danger");
 } else {
-    // Fetch dog details from the database
-    $stmt = $db->prepare("SELECT * FROM Dogs WHERE id = :dog_id");
+    // Check if the logged-in user has permission to view the dog's details
+    $user_id = get_user_id(); // Assuming a function like get_user_id() is defined
+    $stmt = $db->prepare("SELECT * FROM Dogs WHERE id = :dog_id AND user_id = :user_id");
     $stmt->bindValue(':dog_id', $dog_id, PDO::PARAM_INT);
+    $stmt->bindValue(':user_id', $user_id, PDO::PARAM_INT);
     $stmt->execute();
     $dogDetails = $stmt->fetch(PDO::FETCH_ASSOC);
 
     if (!$dogDetails) {
-        // Redirect back to the list page with a flash message for invalid ID
-        flash("Invalid Dog ID", "danger");
+        flash("Invalid Dog ID or You don't have permission to view to this dog.", "danger");
         header("Location: home.php"); 
         exit();
     }
 
-    // Display dog details with styling
     echo "<h2 class='mb-4'>Dog Details</h2>";
     echo "<img src='{$dogDetails['image_url']}' class='img-fluid rounded' alt='Dog Image' style='max-width: 400px;'>";
     echo "<div class='card mt-4' style='width: 18rem; margin: 0 auto;'>";
@@ -39,20 +39,18 @@ if (!$dog_id) {
     echo "<p class='card-text'>HP: {$dogDetails['hp']}</p>";
     echo "<p class='card-text'>Attack: {$dogDetails['attack']}</p>";
     echo "<p class='card-text'>Defense: {$dogDetails['defense']}</p>";
-    // Add Edit and Delete buttons
     echo "<div class='mt-4'>";
-    // Modify the Edit button link to include the dog_id parameter
     echo "<a href='edit.php?dog_id={$dog_id}' class='btn btn-warning mx-2' style='min-width: 80px;'>Edit</a>";
-    // Add a form for the delete functionality
     echo "<form action='delete.php' method='POST' style='display:inline; background-color: transparent; border: none; box-shadow: none;'>";
     echo "<input type='hidden' name='dog_id' value='{$dog_id}'>";
     echo "<button type='submit' class='btn btn-danger mx-2' style='min-width: 80px; border: none;'>Delete</button>";
     echo "</form>";
     echo "</div>";
 
-echo "</div>";
-echo "</div>";
+    echo "</div>";
+    echo "</div>";
 }
+
 echo "</div>";
 
 require(__DIR__ . "/../../partials/flash.php");
